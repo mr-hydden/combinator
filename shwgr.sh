19e7c25b94d2e169e78b559df760072427c0807c
Henry Jookill <mrhydden@thevoid.domainName>
2017-10-26 17:32:10 +0200

    Created script to show group members. Suitable for conversion into bash function

---
+++ shwgr.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+RELEASE_DATE=$(date +'%b-%Y')
+VERSION='v1.0'
+
+echo
+echo
+echo
+echo
+echo
+echo
+echo
+echo '                            Combinator'
+echo '                      '"$VERSION"'              '"$RELEASE_DATE"
+echo
+echo
+echo
+echo
+echo '    Copyright Samuel Gómez Sánchez, Luis Blázquez Miñambres,'\
+     $(date +'%Y')
+echo '                       All Rights Reserved.'
+echo
+echo
+echo
+echo
+
+exit 0
