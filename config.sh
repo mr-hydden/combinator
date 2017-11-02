16ab6b5bcf9898d216498dba6497fd3502ec477f
Henry Jookill <mrhydden@thevoid.domainName>
2017-11-02 21:35:54 +0100

    First version of changePath completed. changeLength in development

--- config.sh
+++ config.sh
@@ -15,13 +15,14 @@
 CONFIG_FILE='conf.cfg'
 CONFIG_FILE_PATH=$PWD
 STATS_FILE='estadisticas.txt'
-R_PERM=114 # r = 114 ASCII
-W_PERM=119 # r = 119 ASCII
-X_PERM=120 # r = 120 ASCII
-RW_PERM=$(( $R_PERM ^ $W_PERM )) # r XOR w = 0x04
-RX_PERM=$(( $R_PERM ^ $X_PERM )) # r XOR x = 0xf2
-WX_PERM=$(( $W_PERM ^ $X_PERM )) # w XOR x = 0xf7
-RWX_PERM=$(( $R_PERM ^ $W_PERM ^ $X_PERM )) # r XOR w XOR x = 0x7d
+declare -i N_PERM=2#000    # ---
+declare -i R_PERM=2#100    # r--
+declare -i W_PERM=2#010    # -w-
+declare -i X_PERM=2#001    # --x
+declare -i RW_PERM=2#110   # rw-
+declare -i WX_PERM=2#011   # -wx
+declare -i RX_PERM=2#101   # r-x
+declare -i RWX_PERM=2#111  # rwx
 
 # Input variables
 NEW_STATS_FILE=
@@ -30,21 +31,76 @@ NEW_LENGTH=
 
 
 
+# ***********************************************
+# binpermf                                      *
+# ***********************************************
+# Convierte una cadena de permisos en un numero *
+# binario que devuelve
+#                                               *
+            function binpermf() {                         
+# ***********************************************
+    #
+    # Return:
+    #   0 si tiene exito
+    #   8 si el formato de argumentos es incorrecto
+    #   9 si la cadena de permisos no es viable
+    #
+    
+    if [ $# -eq 1 ]; then
+        case $1 in
+            'rwx' )
+                return $RWX_PERM
+                ;;
+            'rw-' ) 
+                return $RW_PERM
+                ;;
+            'r-x' )
+                return $RX_PERM
+                ;;
+            'r--' )
+                return $R_PERM
+                ;;
+            '-wx' )
+                return $WX_PERM
+                ;;
+            '-w-' )
+                return $W_PERM
+                ;;
+            '--x' )
+                return $X_PERM
+                ;;
+            '---' )
+                return $N_PERM
+                ;;
+            * )
+                return 9 # Cadena de permisos inexistente
+                ;;
+        esac
+
+    else
+        return 8 # Formato de argumentos incorrecto
+    fi
+}
+
+
 
 # ***********************************************
 # testperm                                      *
 # ***********************************************
-# Tests permissions of $1                       *
+# Comprueba los permisos del fichero o          *
+# o directorio $1                               *
 #                                               *
             function testperm() {                         
 # ***********************************************
     #
     # Return: 
-    #   0 on success
-    #   1 if error (file not existent, can't read on PWD or no arguments)
+    #   0 si tiene exito
+    #   1 si no se reciben argumentos, o se recibe mas de uno
+    #   2 si el fichero no existe o no se puede leer
     #
-    # Echoes a number corresponding to permission change for
-    # current user, for example 6 means rw-
+    # Si todo va bien, hace echo con el numero correspondiente a los permisos
+    # del usuario actual, por ejemplo 6 significa rw- (110 en binario)
+    # En caso de error, el echo es 8 (1000, valor invalido en otro caso)
     #
 
     if [ $# -eq 1 ]; then
@@ -52,24 +108,28 @@ NEW_LENGTH=
         local FILE=$1
         local PERMISSIONS=0
 
-        if ! [[ -r "$PWD" && -e "$FILE" ]]; then
-            return 1
+        if ! [[ -e "$FILE" ]]; then
+            echo 8 # 2#1000, valor invalido; fichero no existe
+            return 2
         fi
 
 
         if [ -r "$FILE" ]; then
-            (( PERMISSIONS += 2#100 )) # Add r-- to permissions
+            # Los permisos hay que ponerlos negados porque indican que permisos
+            (( PERMISSIONS += $R_PERM )) # Incluimos permiso r--
         fi
         if [ -w "$FILE" ]; then
-            (( PERMISSIONS += 2#010 )) # Add -w- to permissions
+            (( PERMISSIONS += $W_PERM )) # Incluimos permiso -w-
         fi
         if [ -x "$FILE" ]; then
-            (( PERMISSIONS += 2#001 )) # Add --x to permissions
+            (( PERMISSIONS += $X_PERM )) # Incluimos permiso --x
         fi
 
         echo $PERMISSIONS
         return 0
+
     else
+        echo 8 # 2#1000, valor invalido; formato de argumentos erroneo
         return 1
     fi
 }
@@ -79,52 +139,90 @@ NEW_LENGTH=
 # ***********************************************
 # init_conf_file                                *
 # ***********************************************
-# Creates default conf.cfg file on $1           *
+# Crea el fichero de configuracion con nombre   *
+# con nombre $1 en el directorio de ruta $2     *
 #                                               *
             function init_conf_file() {                         
 # **********************************************
+#
+    # Return:
+    #   0 si tiene exito
+    #
+    #   1 si recibe argumentos erroneos
+    #
+    #   2 si no se pudo crear el fichero o directorio
+    #
+    #   3 si no se tiene permisos de lectura y escritura en el directorio $2
+    #   es decir, no se puede escribir en el fichero, leerlo, etc.
+    #
+    #   Codigo de permiso: R_PERM, W_PERM o RW_PERM si falta alguno para el 
+    #                      fichero
+
     local DIR=
     local CONFIG_FILE=
 
     if [ $# -eq 2 ]; then
-        DIR=$1
-        CONFIG_FILE=$2
-        if [[ -d "$DIR" && -w "$DIR" && -r "$DIR" ]]; then
 
-            touch "$DIR"/"$CONFIG_FILE"
+        CONFIG_FILE=$1
+        DIR=$2
 
-            # The following is ugly, but testperm can't be done on a file
-            # before knowing if we can read the folder that contains it
-            # or whether it exists.
-            # We must check permissions because we don't know the
-            # system's umask; checking it is approximately as costly
-            # as checking this
-            local FIL_PERM=$(testperm "$CONFIG_FILE")
-            UMASK=2#110 # Same permissions needed
-
-            if (( (( $FIL_PERM & $UMASK )) == 2#010 )); then
-                echo $R_PERM
-                return 1
+        if ! [[ -e "$DIR" ]]; then
+            mkdir "$DIR"
+            if (( $? != 0 )); then  # Si el directorio no se creo correctamente
+                return 2            # (porque no hay permisos, e.g.)
             fi
-            if (( (( $FIL_PERM & $UMASK )) == 2#100 )); then
-                echo $W_PERM
-                return 1
-            fi
-            if (( (( $FIL_PERM & $UMASK )) == 2#000 )); then
-                echo $RW_PERM
-                return 1
+        fi
+        
+        declare -i DIR_PERM=`testperm "$DIR"`
+        if (( $DIR_PERM == $RWX_PERM )); then
+
+            cd "$DIR"
+
+            if ! [ -e "$CONFIG_FILE" ]; then
+                touch "$CONFIG_FILE"
+                if (( $? != 0 )); then # Si el fichero no se creo correctamente
+                      return 2
+                fi
             fi
 
+            local FIL_PERM=$(testperm "$CONFIG_FILE") # En principio
+                                                      # no hace falta comprobar
+                                                      # el valor de retorno,
+                                                      # es mas una medida de 
+                                                      # seguridad, o en caso
+                                                      # de necesitar debugging
+                
+            # Comprobamos cuales son los permisos recibidos de testperm
+            # haciendo AND a nivel de bit con el resultado. Como la mascara
+            # que pasamos es 110, si se apaga alguno de los bits mas
+            # significativos, es que falta algun permiso.
+            # Tenemos que hacerlo porque no conocemos el umask del sistema
+            # y parsearlo sería más o menos igual de costoso
+
+            declare -i UMASK=$RW_PERM   # Necesitamos los mismos permisos 
+                                        # para leer y editar el fichero
+
+            if (( (( $FIL_PERM & $UMASK )) == $W_PERM )); then
+                return $R_PERM # Faltan permisos de lectura
+            fi
+            if (( (( $FIL_PERM & $UMASK )) == $R_PERM )); then
+                return $W_PERM # Faltan permisos de escritura
+            fi
+            if (( (( $FIL_PERM & $UMASK )) == $N_PERM )); then
+                return $RW_PERM # No hay permisos de lectura ni de escritura
+            fi
+            
+            # Escribimos el fichero en el formato adecuado
             echo 'LONGITUD=0' | cat > "$CONFIG_FILE"
-            echo "ESTADISTICAS=$PWD/$STATS_FILE" | cat >> "$CONFIG_FILE"
+            echo "ESTADISTICAS=$PWD/estadisticas.txt" | cat >> "$CONFIG_FILE"
 
             return 0
 
         else
-            return 1
+            return 3 # No hay permisos en el directorio
         fi
     else
-        return 1
+        return 1 # Argumentos erroneos en numero
     fi
 }
 
@@ -139,7 +237,7 @@ NEW_LENGTH=
 # ***********************************************
 
     # Needs 2 mandatory arguments: a path to a file and a string
-    # Usage: changePath PATH STRING
+    # Usage: changePath FILE STRING
     # Changes the configuration file FILE, which has a format as follows:
     #
     # File structure
@@ -155,102 +253,129 @@ NEW_LENGTH=
     # if it does not. However, it does not check validity of STRING
     #
     # Return values:
+    #   0 si tiene exito
     #
+    #   1 si los argumentos son invalidos (en numero)
     #
+    #   2 si el fichero de configuracion no existe y no se puede crear
 
-    local CONFIG_FILE_PATH=
+    local CONFIG_FILE=
     local NEW_STATS_FILE=
 
-    if [ $# -e 2 ]; then
+    if [ $# -eq 2 ]; then
 
-        CONFIG_FILE_PATH=$1     # These are the local variables
-        NEW_STATS_FILE=$2  #
+        CONFIG_FILE=$1
+        NEW_STATS_FILE=$2
+        # Separamos la ruta y el nombre del fichero, para poder trabajar con
+        # testperm y comprobar los permisos por separado
+        CONFIG_FILE_PATH=`echo $CONFIG_FILE | sed -n 's:\(.*\)/.*$:\1:p'`
+        CONFIG_FILE_NAME=`echo $CONFIG_FILE | sed -n 's:.*/\(.*\)$:\1:p'`
     
-        local DIR_PERM=$(testperm "$CONFIG_FILE_PATH")
-        local UMASK=2#110   # To test if we have read and write permissions,
-                            # which we need. If DIR_PERM & UMASK != 110, 
-                            # one or both of those is 0
+        local DIR_PERM=`testperm "$CONFIG_FILE_PATH"`
+        declare -i local UMASK=2#110    # To test if we have read and write permissions,
+                                        # which we need. If DIR_PERM & UMASK != 110, 
+                                        # one or both of those is 0
 
         if (( (( $DIR_PERM & $UMASK )) == 2#010 )); then
-            echo $R_PERM
-            return 1
+            return $R_PERM
         fi
         if (( (( $DIR_PERM & $UMASK )) == 2#100 )); then
-            echo $W_PERM
-            return 1
+            return $W_PERM
         fi
         if (( (( $DIR_PERM & $UMASK )) == 2#000 )); then
-            echo $RW_PERM
-            return 1
+            return $RW_PERM
         fi
         
-        if [[ ! -e "$CONFIG_FILE_PATH"/"$CONFIG_FILE" ]]; then
-            if ! init_conf_file "$CONFIG_FILE_PATH" "$CONFIG_FILE"; then
-                return 1
+        if [[ ! -e "$CONFIG_FILE_PATH"/"$CONFIG_FILE_NAME" ]]; then
+            if ! init_conf_file "$CONFIG_FILE_NAME" "$CONFIG_FILE_PATH"
+            then
+                return 2 # Fichero de configuracion no se puede crear
             fi
         fi
 
-        sed -i "2s:ESTADISTICAS=.*:ESTADISTICAS=$NEW_STATS_FILE" \
-        "$CONFIG_FILE_PATH"/"$CONFIG_FILE"
+        sed -i "2s:ESTADISTICAS=.*:ESTADISTICAS=$NEW_STATS_FILE:" \
+        "$CONFIG_FILE_PATH"/"$CONFIG_FILE_NAME"
+
+        return 0
+
     else
-        return 1
+        return 1 # Numero de argumentos no valido
     fi
 }
 
 
 
+# *********************************************** # REVISAR Y COMPLETAR <---------------------------------------------------
+# changeLength()                                *
+# ***********************************************
+# Function to change length in config file      *
+#                                               *
+            function changeLength() {                         
+# ***********************************************
 
+    # Needs 2 mandatory arguments: a path to a file and a string
+    # Usage: changeLength FILE N
+    # Changes the configuration file FILE, which has a format as follows:
+    #
+    # File structure
+    #==============================================================
+    #LONGITUD=N
+    #ESTADISTICAS=/path/to/statistics/file
+    #^D 
+    #==============================================================
+    #
+    # /path/to/statistics/file will become STRING
+    #
+    # This function testes if configuration file exists, and creates it
+    # if it does not. However, it does not check validity of STRING
+    #
+    # Return values:
+    #   0 si tiene exito
+    #
+    #   1 si los argumentos son invalidos (en numero)
+    #
+    #   2 si el fichero de configuracion no existe y no se puede crear
 
-# FROM HERE TO END NOT TESTED. UPPER FUNCTIONS ARE WORKING AND TESTED.
-while :
-do
-    case $1 in
-        -l | --length) # To change length
-            if [[ $2 =~ [0-9] ]]; then
-                NEW_LENGTH=$2
-                shift 2
-            else
-                echo "config: invalid argument '$2' for option '$1'" >&2
-                exit 1
-            fi
-            ;;
+    local CONFIG_FILE=
+    local NEW_LENGTH=
+
+    if [ $# -eq 2 ]; then
+
+        CONFIG_FILE=$1
+        NEW_LENGTH=$2
+        # Separamos la ruta y el nombre del fichero, para poder trabajar con
+        # testperm y comprobar los permisos por separado
+        CONFIG_FILE_PATH=`echo $CONFIG_FILE | sed -n 's:\(.*\)/.*$:\1:p'`
+        CONFIG_FILE_NAME=`echo $CONFIG_FILE | sed -n 's:.*/\(.*\)$:\1:p'`
+    
+        local DIR_PERM=`testperm "$CONFIG_FILE_PATH"`
+        declare -i local UMASK=2#110    # To test if we have read and write permissions,
+                                        # which we need. If DIR_PERM & UMASK != 110, 
+                                        # one or both of those is 0
+
+        if (( (( $DIR_PERM & $UMASK )) == 2#010 )); then
+            return $R_PERM
+        fi
+        if (( (( $DIR_PERM & $UMASK )) == 2#100 )); then
+            return $W_PERM
+        fi
+        if (( (( $DIR_PERM & $UMASK )) == 2#000 )); then
+            return $RW_PERM
+        fi
         
-        -s | --statsfile) # To change statistics file
-            if [[ $2 =~ .*/"$STATS_FILE" ]]; then
-                NEW_STATS_FILE=$2
-                shift 2
-            else
-                echo "config: invalid argument '$2' for option '$1'" >&2
-                echo "config: statistics file must be named '$STATS_FILE'"
-                exit 1
+        if [[ ! -e "$CONFIG_FILE_PATH"/"$CONFIG_FILE_NAME" ]]; then
+            if ! init_conf_file "$CONFIG_FILE_NAME" "$CONFIG_FILE_PATH"
+            then
+                return 2 # Fichero de configuracion no se puede crear
             fi
-            ;;
+        fi
 
-        -*)
-            echo "config: invalid option '$1'" >&2
-            echo "Usage: config [-l | --length N] [-s | --statsfile FILE]" >&2
-            echo "Try 'config --help' for more information" >&2
-            exit 1
-            ;;
-        *)
-            break
-            ;;
-    esac
-done
+        sed -i "2s:LONGITUD=.*:LONGITUD=$NEW_LENGTH:" \
+        "$CONFIG_FILE_PATH"/"$CONFIG_FILE_NAME"
 
+        return 0
 
-
-
-
-
-if [ -n "$NEW_STATS_FILE" -a -n "$NEW_LENGTH" ]; then
-        # CHANGE PATH AND LENGHT
-else
-
-    if [ -n "$NEW_LENGTH" ]; then
-        # CHANGE LENGTH
+    else
+        return 1 # Numero de argumentos no valido
     fi
-    if [ -n "$NEW_STATS_FILE" -a -n "$NEW_LENGTH" ]; then
-        # CHANGE PATH
-    fi
-fi
+}
