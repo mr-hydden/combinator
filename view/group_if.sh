eb3df286098c58e80084fc7189299d44b262dbb6
Henry Jookill <mrhydden@thevoid.domainName>
2017-11-05 17:04:33 +0100

    Added wait to group_if.sh

--- view/group_if.sh
+++ view/group_if.sh
@@ -10,18 +10,23 @@ echo
 echo
 echo
 echo
-echo '                            Combinator'
-echo '                      '"$VERSION"'              '"$RELEASE_DATE"
+echo
+echo
+echo '                                Combinator                                '
+echo '                          '"$VERSION"'              '"$RELEASE_DATE"'     '
 echo
 echo
 echo
 echo
-echo '    Copyright Samuel Gómez Sánchez, Luis Blázquez Miñambres,'\
-     $(date +'%Y')
-echo '                       All Rights Reserved.'
+echo '       Copyright Samuel Gómez Sánchez, Luis Blázquez Miñambres, '\
+$(date +'%Y') '      '
+echo '                            All Rights Reserved.                          '
 echo
 echo
 echo
 echo
+echo
+echo
+read -p 'Pulse RETURN para volver al menu principal...' A
 
 exit 0
